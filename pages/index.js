c0cf457fc53b070149798bd9c44d44482c75a9ac
chenzhixiang <chenzhixiang675784@163.com>
2022-08-09 15:04:05 +0800

    initial

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.scss'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+// export default function Home() {
+//   return (
+//     <Layout home>
+//       <Head>
+//         <title>{siteTitle}</title>
+//       </Head>
+//       <section className={utilStyles.headingMd}>
+//         <p>[Your Self Introduction]</p>
+//         <p>
+//           (This is a sample website - you’ll be building a site like this on{' '}
+//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+//         </p>
+//         <Link href="/posts/first-post">
+//           <a>去post</a>
+//         </Link>
+//       </section>
+//     </Layout>
+//   );
+// }
